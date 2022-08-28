b3c9513d7f390676341483ddba9114cb21845dd3
hinamei <hina.gntm@gmail.com>
2022-08-28 13:01:08 +0900

    スレッドコンポーネントにスレッド一覧取得機能を追加

--- src/Thread.tsx
+++ src/Thread.tsx
@@ -1,17 +1,43 @@
 import './index.css';
-import ThreadList from './ThreadList';
+import {useState} from 'react'
+import {useEffect} from 'react'
 
 function Thread() {
-  return (
+	// スレッド情報を保持する
+	const[threads, setThreads] = useState<string[]>([]);
+	const threadList: string[] = [];
+
+	// APIにアクセスして、スレッド一覧を取得する
+	useEffect(() => {
+		fetch("https://railway-react-bulletin-board.herokuapp.com/threads")
+		.then(res => res.json())
+		.then(data => {
+				for(let i = 0; i <data.length; i++){
+						threadList.push(data[i].title);
+				}
+				setThreads(threadList);
+				console.log(threadList)
+		});
+	} , []);
+
+  const threadRecord = threads.map((threadTitle) =>{
+		return(
+			<tr className='bg-white'>
+				<td className='border text-xl'>{threadTitle}</td>
+			</tr>
+		)
+	})
+
+	return (
     <div className='pt-5'>
-        <table  className='table-fixed'>
-            <thead>
-                <tr>
-                    <th className='w-96 text-2xl'>新着スレッド</th>
-                </tr>
-            </thead>
-            <ThreadList />
-        </table>
+			<table  className='table-fixed'>
+				<thead>
+						<tr>
+								<th className='w-96 text-2xl'>新着スレッド</th>
+						</tr>
+				</thead>
+				{threadRecord}
+			</table>
     </div>
   );
 }
