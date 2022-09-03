61d48db4bdc91d95c102adcf1fc90bccebf84f6f
hinamei <hina.gntm@gmail.com>
2022-09-03 23:39:49 +0900

    mainタグを追加

--- src/Content.tsx
+++ src/Content.tsx
@@ -3,11 +3,11 @@ import Thread from './Thread';
 
 function Content() {
   return (
-    <div className='h-screen w-screen bg-neutral-300'>
+    <main className='h-screen w-screen bg-neutral-300'>
         <div className='flex flex-col justify-center items-center'>
             <Thread />
         </div>
-    </div>
+    </main>
   );
 }
 
